d8e93fcc6bef9324ebc83105caacd57e681d5e26
Neekon <neekon91@gmail.com>
2016-09-05 14:21:28 -0700

    mod-server-routes closes #33

--- server/server.js
+++ server/server.js
@@ -1,38 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+router = require('./routes');
 
 
 app.use(express.static(__dirname + '/../client/'));
 app.use(bodyParser.json());
 app.use('/api', router);
-
 //////////////////////////////////////////
 //                                      //
 //               Routes                 //
 //                                      //
 //////////////////////////////////////////
 
-router.route('/resources')
-  .post(function(req, res){
-    res.send('post request received')
-  })
-  .get(function(req, res){
-    res.send('get request received')
-  });
-
-
-
-router.route('/resource/:resource_id')
-  .get(function(req, res){
-    res.send('singlet get req. received')
-  })
-  .put(function(req, res){
-    res.send('update req. received')
-  })
-  .delete(function(req, res){
-    res.send('delete request received')
-  })
 
 
 app.listen(process.env.PORT || 3000);
