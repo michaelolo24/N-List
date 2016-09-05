c7c5c0012d2cc0783bbf82f994bdfed4723c8446
Neekon <neekon91@gmail.com>
2016-09-05 16:23:20 -0700

    null db closes #39

--- schema.sql
+++ schema.sql
@@ -16,12 +16,12 @@ USE rezzy;
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT,
   `name` VARCHAR(50) NOT NULL,
   `email` VARCHAR(55) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  `bookmarked` INTEGER NULL DEFAULT NULL,
-  `languages` INTEGER NULL DEFAULT NULL,
+  `bookmarked` INTEGER DEFAULT NULL,
+  `languages` INTEGER DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -33,8 +33,8 @@ CREATE TABLE `users` (
 DROP TABLE IF EXISTS `creators`;
 
 CREATE TABLE `creators` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(55) NULL DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT,
+  `name` VARCHAR(55) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -46,16 +46,16 @@ CREATE TABLE `creators` (
 DROP TABLE IF EXISTS `resources`;
 
 CREATE TABLE `resources` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_creators` INTEGER NULL DEFAULT NULL,
-  `id_languages` INTEGER NULL DEFAULT NULL,
-  `id_resource_type` INTEGER NULL DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT,
+  `id_creators` INTEGER DEFAULT NULL,
+  `id_languages` INTEGER DEFAULT NULL,
+  `id_resource_type` INTEGER DEFAULT NULL,
   `link` VARCHAR(255) NOT NULL,
   `date_added` TIMESTAMP NOT NULL,
   `date_created` TIMESTAMP NULL,
   `keywords` VARCHAR(255) NULL,
-  `likes` INTEGER NULL DEFAULT NULL,
-  `dislikes` INTEGER NULL DEFAULT NULL,
+  `likes` INTEGER DEFAULT NULL,
+  `dislikes` INTEGER DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -67,7 +67,7 @@ CREATE TABLE `resources` (
 DROP TABLE IF EXISTS `languages`;
 
 CREATE TABLE `languages` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT,
   `name` INTEGER NOT NULL,
   PRIMARY KEY (`id`)
 );
@@ -80,8 +80,8 @@ CREATE TABLE `languages` (
 DROP TABLE IF EXISTS `resource_type`;
 
 CREATE TABLE `resource_type` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `type` VARCHAR(55) NOT NULL DEFAULT 'NULL',
+  `id` INTEGER AUTO_INCREMENT,
+  `type` VARCHAR(55) NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -102,4 +102,3 @@ ALTER TABLE `resources` ADD FOREIGN KEY (id_resource_type) REFERENCES `resource_
 -- ALTER TABLE `resources` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `languages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `resource_type` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
