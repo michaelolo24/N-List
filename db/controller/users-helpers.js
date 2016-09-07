08057172432d9000572b78652d7c46bbe5be522d
Neekon <neekon91@gmail.com>
2016-09-06 21:47:35 -0700

    modularize db controller helpers closes #101

---
+++ db/controller/users-helpers.js
@@ -0,0 +1,56 @@
+var db = require('../dbConnect/connection.js');
+
+var Users = {
+
+  // ****SIGN IN HELPER ****
+  signIn: function (params, callback) {
+    var data = [params.email, params.password];
+
+    var query = 'SELECT * FROM users WHERE email=? AND password=? LIMIT 1';
+    db.query(query, data, function(err, results) {
+      callback(err, results);
+    });
+  },
+
+   // ****SIGN UP HELPER ****
+  signUp: function (params, callback) {
+    var data = [params.name, params.email, params.password];
+
+    var query = "INSERT INTO users (name,email,password) VALUES (?,?,?)";
+    db.query(query, data, function(err, results) {
+      callback(err, results);
+    });
+  },
+
+   // ****GET USER INFO FOR UPDATE PAGE ****
+  getOne: function (params, callback) {
+    var data = [params.email, params.password];
+
+    var query = 'SELECT * FROM users WHERE email=? AND password=? LIMIT 1';
+    db.query(query, data, function(err, results) {
+      callback(err, results);
+    });
+  },
+
+   // ****UPDATE USER INFO****
+  updateOne: function (params, callback) {
+    var data = [params.name, params.photoPath, (params.languages || null), params.email, params.password];
+
+    var query = 'UPDATE users SET name=?, photo_path=?, languages=?, email=?, password=? WHERE id ="'+params.id+'" LIMIT 1';
+    db.query(query, data, function(err, results) {
+      callback(err, results);
+    });
+  },
+
+  // ****DELETE USER [DESTROY ACCOUNT]****
+  deleteOne: function (params, callback) {
+    var data = [params.email, params.password];
+
+    var query = 'DELETE FROM users WHERE email=? AND password=? LIMIT 1';
+    db.query(query, data, function(err, results) {
+      callback(err, results);
+    });
+  }
+};
+
+module.exports = Users;
