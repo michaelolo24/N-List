49669228c725d7ec8eb603bbc80828cb77e5acdd
Neekon <neekon91@gmail.com>
2016-09-06 23:17:12 -0700

    photo upload connect to front end closes #104

--- client/components/profile/profileCtrl.js
+++ client/components/profile/profileCtrl.js
@@ -1,7 +1,14 @@
 var app = angular.module('nList.profile', ['ngFileUpload']);
 
-app.controller('ProfileController', [ '$scope', 'Upload','$window', function($scope){
+app.controller('ProfileController', [ '$scope', '$http', 'Upload', '$window', function($scope, $http, Upload, $window){
 
+  // var load = function(){
+  //   $http.get('/users/updateUser').success(function(res){
+  //     $scope.userUpdate = res;
+  //
+  //   })
+  // }
+  // load();
   $scope.findPicName = function (){
     var pic = document.getElementById('file').files[0];
     $scope.picName = pic;
@@ -24,5 +31,18 @@ app.controller('ProfileController', [ '$scope', 'Upload','$window', function($sc
       })
   };
 
+  $scope.editProfile = function(){
+    var data = {
+      id:1,
+      name: 'mike',
+      email: 'big O',
+      password: 'safjdh',
+      photoPath: 'http://localhost:3000/uploads/'+$scope.findPicName()
+    }
+    $http.put('/users/updateUser', data).success(function(res){
+      //place photo back up here
+      console.log(res);
+    })
+  }
 
 }])
