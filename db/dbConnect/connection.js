fc78eb0745fcf152fe34b467cf6478e765b64d0c
djwirz <djwirz@gmail.com>
2016-09-08 16:24:16 -0500

    deploy_AWS closes #6

--- db/dbConnect/connection.js
+++ db/dbConnect/connection.js
@@ -1,19 +1,20 @@
 var mysql = require('mysql');
 
+// var dbCon = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'rezzy'
+// });
+
 var dbCon = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'rezzy'
+  host: 'nlist.cvtsvwvdzejt.us-west-1.rds.amazonaws.com',
+  user: 'Neekon',
+  password: 'NList11.0',
+  database: 'NList',
+  port: 3306
 });
 
-// var dbCon = mysql.createConnection({
-//   host: 'nlist.cjr98onwco2a.us-west-2.rds.amazonaws.com',
-//   user: 'Neekon',
-//   password: 'NList11.0',
-//   database: 'NList',
-//   port: 3306
-// });
 dbCon.connect(function(err) {
   if (err) {
     console.log('error did not connect to rezzy CHECK YO self')
