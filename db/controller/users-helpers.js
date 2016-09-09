fe04657806557cc4ff99b0af9b61e243ffe88766
Neekon <neekon91@gmail.com>
2016-09-08 21:05:20 -0700

    es6 users and links closes #172

--- db/controller/users-helpers.js
+++ db/controller/users-helpers.js
@@ -3,59 +3,47 @@ var db = require('../dbConnect/connection.js');
 var Users = {
 
   // ****SIGN IN HELPER ****
-  signIn: function (params, callback) {
+  signIn:  (params, callback) => {
     var data = [params.email];
     var query = 'SELECT * FROM users WHERE email=? LIMIT 1';
-    db.query(query, data, function(err, results) {
-      callback(err, results);
-    });
+    db.query(query, data, (err, results) => callback(err, results) );
   },
 
    // ****SIGN UP HELPER ****
-  signUp: function (params, callback) {
+  signUp:  (params, callback) => {
     var data = [params.name, params.email, params.password];
     var query = "INSERT INTO users (name,email,password) VALUES (?,?,?)";
-    db.query(query, data, function(err, results) {
-      callback(err, results);
-    });
+    db.query(query, data, (err, results) => callback(err, results) );
   },
 
   // ****CHECK USER HELPER ****
-  checkUser: function (params, callback) {
+  checkUser:  (params, callback) => {
     var data = [params.email];
     var query = "SELECT * FROM users WHERE email=? LIMIT 1";
-    db.query(query, data, function(err, results) {
-      callback(err, results);
-    });
+    db.query(query, data, (err, results) => callback(err, results) );
   },
 
    // ****GET USER INFO FOR UPDATE PAGE ****
-  getOne: function (params, callback) {
+  getOne:  (params, callback)=> {
     // var data = [params.email, params.password];
     var query = 'SELECT * FROM users WHERE id=? LIMIT 1';
-    db.query(query, [params], function(err, results) {
-      callback(err, results);
-    });
+    db.query(query, [params], (err, results)=> callback(err, results) );
   },
 
    // ****UPDATE USER INFO****
-  updateOne: function (params, callback) {
+  updateOne:  (params, callback) => {
     var data = [params.name, params.photoPath, (params.languages || null), params.email, params.password];
     console.log(params);
     var query = 'UPDATE users SET name=?, photo_path=?, languages=?, email=?, password=? WHERE id ="'+params.id+'" LIMIT 1';
-    db.query(query, data, function(err, results) {
-      callback(err, results);
-    });
+    db.query(query, data, (err, results) => callback(err, results) );
   },
 
   // ****DELETE USER [DESTROY ACCOUNT]****
-  deleteOne: function (params, callback) {
+  deleteOne:  (params, callback) => {
     var data = [params.email, params.password];
 
     var query = 'DELETE FROM users WHERE email=? AND password=? LIMIT 1';
-    db.query(query, data, function(err, results) {
-      callback(err, results);
-    });
+    db.query(query, data, (err, results) => callback(err, results) );
   }
 };
 
